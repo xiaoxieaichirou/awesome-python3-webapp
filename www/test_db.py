e8bc260a3193453f1994eeedd9759c7097ef02bf
xiaoxieaichirou <13226512215@163.com>
2021-03-28 16:45:59 +0800

    新增orm\app代码

---
+++ www/test_db.py
@@ -0,0 +1,16 @@
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+async def test(loop):
+	await orm.create_pool(loop=loop, user='root', password='admin123', db='test')
+	u = User(name='Test', email='test@qq.com', passwd='1234567890', image='about:blank')
+	await u.save()
+
+	orm.__pool.close()
+	await orm.__pool.wait_closed()
+
+if __name__ == '__main__':
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(test(loop))
+	loop.close()
